b93e25ad8cb02e24ae7cf2e3500f49763e03bd6d
Student <student@RU-005.local>
2016-01-25 17:25:29 -0700

    I dunno

--- main.js
+++ main.js
@@ -3,18 +3,13 @@ angular.module('modRush', [])
 angular.module('modRush')
 	.controller('goldRoller', ['$scope', function($scope){
 		$scope.coordinates = []
-		$scope.showText = false
+		$scope.showText= true
 		$scope.markSpot = function(event) {
-			
-			$scope.showText = true
 			$scope.spot = {
 				'left': event.clientX.toString()+"px",
 				'top': event.clientY.toString()+"px",
-				'message': ''
 			}
-			console.log("spot!")
 			$scope.coordinates.push($scope.spot)
-			console.log($scope.coordinates)
 		}
 		$scope.deleteSpot = function(index) {
 			$scope.coordinates.splice(index, 1)
