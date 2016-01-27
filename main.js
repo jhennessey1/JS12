901eb020ce054c63dcf0b936ab639efbf0f6a5a3
Student <student@RU-005.local>
2016-01-26 17:33:45 -0700

    Everything is wrong

--- main.js
+++ main.js
@@ -3,23 +3,39 @@ angular.module('modRush', [])
 angular.module('modRush')
 	.controller('goldRoller', ['$scope', function($scope){
 		$scope.coordinates = []
-		$scope.showText= true
+		$scope.comments = {}
 		$scope.markSpot = function(event) {
-			$scope.spot = {
-				'left': event.clientX.toString()+"px",
-				'top': event.clientY.toString()+"px",
+			$scope.markObject = {
+				'coords' : { 
+					'left': (event.clientX - 5).toString()+"px",
+				  	'top': (event.clientY - 5).toString()+"px" 
+				  },
+				'left': (event.clientX - 5).toString()+"px",
+				'top': (event.clientY - 5).toString()+"px",
+				'comment':'',
+				'showComment': false
 			}
-			$scope.coordinates.push($scope.spot)
+			$scope.coordinates.push($scope.markObject)
 		}
 		$scope.deleteSpot = function(index) {
 			$scope.coordinates.splice(index, 1)
 		}
-		$scope.submitComment = function(event) {
-			$scope.showText = false
-			$scope.coordinates[$scope.coordinates.length - 1].message = 'textarea data'
-			// pass data from textarea into function
-			// modify spot object while it is in $scope.coordinates
+		
+		$scope.hideTextarea = false
+		$scope.subComment = function(index) {
+			$scope.comments.index = $scope.comments
+			$scope.commentSpots.splice(index, 1)
+			console.log(comments[index])
 		}
 
-		// to do: format textarea div
+		
+
+		$scope.commentsOn = function(index) {
+			$scope.showComment[index] = true
+		}
+
+		$scope.commentsOff = function(index) {
+			$scope.showComment[index] = false
+		}
+
 	}])
